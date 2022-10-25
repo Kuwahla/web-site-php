dcf81495382a2e72e09818b45f123f42fb4d5426
Mike-Proven <m.proven.it@gmail.com>
2022-10-25 16:59:58 -0700

    div1

--- index.php
+++ index.php
@@ -97,15 +97,15 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
   </div>
 
   <!-- Portfolio Section -->
-<div class="w3-half">
   <div class="w3-padding-32 w3-content" id="photos">
+  <div class="w3-half">
     <h2 class="w3-text-light-grey">Food Decision App</h2>
     <hr style="width:200px" class="w3-opacity">
 
     <a href="https://play.google.com/store/apps/details?id=solo.miketheappsmith.fooddecisionapp" target="_blank"><img src="/pics/FDA.png" style="width:64;height:64;"></a> 
-  </div>
-  <div><h1>???</h1>
-</div>
+  </div><div class="w3-half">
+  <h1>???</h1>
+  </div></div>
 
   <!-- Contact Section -->
   <div class="w3-padding-16 w3-content w3-text-grey" id="contact">
